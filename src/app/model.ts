143a66b4c9fada533b6e7bcc754826bb1fbde290
fogelkuba <fogelkuba@gmail.com>
2018-07-15 19:34:09 +0200

    fix

--- src/app/model.ts
+++ src/app/model.ts
@@ -6,3 +6,25 @@
 //     {action: "kupic bilety", done: false},
 //     ]
 // };
+
+export class Model {
+  user;
+  items;
+  constructor() {
+    this.user = 'Adam';
+    this.items = [
+      {action: "Kupic kwiaty", done: false},
+      {action: "kupic buty", done: true},
+      {action: "kupic bilety", done: false},
+    ];
+  }
+}
+
+export class TodoItem {
+  action;
+  done;
+  constructor(action, done){
+    this.action = action;
+    this.done = done;
+  }
+}
