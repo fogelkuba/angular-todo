7d6834e4a8223ec35dbf4cc4d93a47db6505bd77
fogelkuba <fogelkuba@gmail.com>
2018-07-15 18:57:33 +0200

    fix

---
+++ src/app/model.ts
@@ -0,0 +1,8 @@
+// var model = {
+//   user: Adam,
+//   items: [
+//     {action: "Kupic kwiaty", done: false},
+//     {action: "kupic buty", done: true},
+//     {action: "kupic bilety", done: false},
+//     ]
+// };
